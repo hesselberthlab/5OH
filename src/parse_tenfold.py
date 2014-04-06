b7399f1fb6c2d96cff9c6cad1fa2801ce5b2e54b
jayhesselberth <jay.hesselberth@gmail.com>
2014-04-06 12:48:28 -0600

    changed tabs to spaces

--- src/parse_tenfold.py
+++ src/parse_tenfold.py
@@ -6,24 +6,24 @@ import re
 import sys
 
 def parse_tenfold(tabfile):
-	"""Convert tenfold.py output to BED output
-	"score" is tenfold output:
-	1 = double-stranded
-	2 = single-standed
-	"""
-	for line in open(tabfile):
-		label, secondary = line.strip().split("\t")
+    """Convert tenfold.py output to BED output
+    "score" is tenfold output:
+    1 = double-stranded
+    2 = single-standed
+    """
+    for line in open(tabfile):
+        label, secondary = line.strip().split("\t")
 
-		split_label = re.findall(r"[\w']+", label)
-		name = split_label[0]
-		chrom, start, stop = split_label[-3:]
+        split_label = re.findall(r"[\w']+", label)
+        name = split_label[0]
+        chrom, start, stop = split_label[-3:]
 
-		start = int(start)
-		stop = int(stop)
+        start = int(start)
+        stop = int(stop)
 
-		for i in range(len(secondary)):
-			print "\t".join([chrom, str(start+i), \
-						str(start+i+1), name, secondary[i]])
+        for i in range(len(secondary)):
+            print "\t".join([chrom, str(start+i), \
+                        str(start+i+1), name, secondary[i]])
 
 parse_tenfold(sys.argv[1])
-		
+        
--- src/plfolding_analysis.py
+++ src/plfolding_analysis.py
@@ -12,58 +12,58 @@ from collections import defaultdict
 from subprocess import Popen, PIPE
 
 def generate_pair_probs(seq):
-	"""Generates pair probabilities for given sequence
-		Params:
-			seq (str): RNA sequence
-		Return:
-			prob_dict (dict): 	key is base index
-								value is highest prob of base pairing (local)
-	"""
-	p = Popen("RNAplfold", stdin=PIPE)		# part of ViennaRNA
-	p.communicate(seq+'\n@')				# input sequence and exit prog
+    """Generates pair probabilities for given sequence
+        Params:
+            seq (str): RNA sequence
+        Return:
+            prob_dict (dict):   key is base index
+                                value is highest prob of base pairing (local)
+    """
+    p = Popen("RNAplfold", stdin=PIPE)      # part of ViennaRNA
+    p.communicate(seq+'\n@')                # input sequence and exit prog
 
-	table_start = False
-	prob_dict = defaultdict(int)
+    table_start = False
+    prob_dict = defaultdict(int)
 
-	# process PostScript output file from RNAplfold and grab probabilities
-	for line in open("plfold_dp.ps"):
-		line = line.strip()
-		if line == "showpage": break		# end of table
-		if line == "drawgrid_turn":			# beginning of table
-			table_start = True
-			continue
-		if table_start:
-			i, j, prob = line.split(" ")[:3]
-			if prob > prob_dict[i]:			# get greatest prob; (!!!not ideal soln)
-				prob_dict[i] = prob
+    # process PostScript output file from RNAplfold and grab probabilities
+    for line in open("plfold_dp.ps"):
+        line = line.strip()
+        if line == "showpage": break        # end of table
+        if line == "drawgrid_turn":         # beginning of table
+            table_start = True
+            continue
+        if table_start:
+            i, j, prob = line.split(" ")[:3]
+            if prob > prob_dict[i]:         # get greatest prob; (!!!not ideal soln)
+                prob_dict[i] = prob
 
-	return prob_dict
+    return prob_dict
 
 def make_bedfile(prob_table, label):
-	"""Create a bedfile output from basepair probabilities and label
-		Params:
-			prob_table (dict) - dictionary of highest prob of base-pairing for given index
-			label (str) - name;chr:start:stop;[+|-]
-		Return:
-			None
-	"""
-	name, coords, strand = label.split(";")
-	chrom, startstop = coords.split(":")
-	start, stop = startstop.split("-")
-	start = int(start)
-	
-	for key in sorted(prob_table.keys()):
-		i = int(key)
-		print "\t".join([chrom, str(i+start-1), str(i+start), prob_table[key], name, strand])
+    """Create a bedfile output from basepair probabilities and label
+        Params:
+            prob_table (dict) - dictionary of highest prob of base-pairing for given index
+            label (str) - name;chr:start:stop;[+|-]
+        Return:
+            None
+    """
+    name, coords, strand = label.split(";")
+    chrom, startstop = coords.split(":")
+    start, stop = startstop.split("-")
+    start = int(start)
+    
+    for key in sorted(prob_table.keys()):
+        i = int(key)
+        print "\t".join([chrom, str(i+start-1), str(i+start), prob_table[key], name, strand])
 
 def main(args=sys.argv[1:]):
-	for line in open(args[0]):
-		label, sequence = line.strip().split("\t")
-		prob_dict = generate_pair_probs(sequence)
-		make_bedfile(prob_dict, label)
+    for line in open(args[0]):
+        label, sequence = line.strip().split("\t")
+        prob_dict = generate_pair_probs(sequence)
+        make_bedfile(prob_dict, label)
 
 if __name__ == '__main__':
-	sys.exit(main()) 
+    sys.exit(main()) 
 
 
 
