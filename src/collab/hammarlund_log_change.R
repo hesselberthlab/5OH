377fe5675baa6d3bc2fcbac576340629e3617c24
Sally Peach <sallypeach@gmail.com>
2014-04-30 19:13:12 -0600

    added legend, additional sort options

--- src/collab/hammarlund_log_change.R
+++ src/collab/hammarlund_log_change.R
@@ -18,25 +18,31 @@ rtcb_ind <- gene_exp %.%
   select(test_id,sample_1,sample_2,value_1,value_2,log2.fold_change.) %.%
   filter(sample_1=="RTCB",
          sample_2=="RTCB_TM",
+         #log2.fold_change.>=1.75,
          value_1>=1,
          value_2>=1)
 
-p_data <- merge(ctrl_ind,rtcb_ind,by="test_id")
+# _02 graph 
+plot_data <- merge(ctrl_ind,rtcb_ind,by="test_id") %.%
+  arrange(desc(log2.fold_change..y))
 
-
+# _01 graph
 plot_data <- merge(ctrl_ind,rtcb_ind,by="test_id")  %.%
-  filter(value_1.x <= value_1.y*10,
-         value_1.x >= value_1.y/10,
-         value_1.y < value_1.x) %.%
+  filter(#value_1.x <= value_1.y*100,
+         #value_1.x >= value_1.y/100,
+         value_1.y <= value_1.x) %.%
   arrange(desc(log2.fold_change..x))
 
-gp <- ggplot(plot_data,aes(x=rev(reorder(test_id, log2.fold_change..x)),y=log2.fold_change..x))
-gp + geom_bar(fill="#CC79A7",stat="identity") +
-  geom_bar(data=plot_data,stat="identity",aes(y=log2.fold_change..y),fill="blue",alpha=0.4) +
-  ylab("Log2 Fold Change") +
+gp <- ggplot(plot_data,aes(x=rev(reorder(test_id, log2.fold_change..y)),y=log2.fold_change..x))
+gp + geom_bar(aes(fill="Control"),stat="identity") +
+  geom_bar(data=plot_data,stat="identity",aes(y=log2.fold_change..y,fill="RtcB"),alpha=0.5) +
+  ylab("Log2 Fold Change, Tm vs. DMSO") +
   xlab("Gene") +
   labs(fill="Strain") +
-  scale_fill_manual(name="Strain",values=c("Control"="#CC79A7","RtcB"="blue"),guide="none") +
+  scale_fill_manual(values=c("lightblue", "#CC79A7"), 
+                    name="Strain",
+                    labels=c("Control", "RtcB")) +
+  scale_y_continuous(breaks=c(-2,0,2,4)) + 
   theme_bw() + 
   theme(axis.text.x = element_text(size=10,angle=90,vjust=0.7, color="black"),
         axis.title.x = element_text(size=14, face="bold"),
