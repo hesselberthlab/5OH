8b559520a69d9b5cf583c00d0daf1313149df54d
Sally Peach <sallypeach@gmail.com>
2014-09-11 12:19:38 -0600

    functional, but suuuuuper slow. :(

--- src/collab/hammarlund-lab/changepoints.py
+++ src/collab/hammarlund-lab/changepoints.py
@@ -5,6 +5,7 @@
 import sys
 import pdb
 import rpy2.robjects as robjects
+import operator
 
 from pybedtools import BedTool
 from rpy2.robjects.packages import importr
@@ -23,21 +24,37 @@ def changepoints(gene_bed, signal_bedgraph):
     genes = BedTool(gene_bed)
     signal = BedTool(signal_bedgraph)
 
+    changepoint_scores = {}
+
     for region in genes:      # loop through each gene
+        gene_name = region.fields[3]
         region_bedtool = BedTool([region.fields[:3]])
-        signal_data = signal.intersect(region_bedtool)
+        signal_data = signal.intersect(region_bedtool) # strandedness argument?
 
         # Create list from count intersection
         count_list = [int(datum.fields[3]) for datum in signal_data]
+        if len(count_list) < 4: continue      # need 4 values to calc changepoint
 
         # Convert to Int Vector
         count_vector = robjects.IntVector(count_list)
 
         region_cpt = calc_changepoint(count_vector)
+        if region_cpt < 2: continue   # if no changepoint found
 
-        pdb.set_trace()
+        # avg signal counts to right of changepoint
+        rcpt_avg = sum(count_vector[region_cpt:])/float(len(count_vector[region_cpt:]))
 
-        # Todo: Store as tuple with region entry.
+        # avg signal counts to left of changepoint
+        lcpt_avg = sum(count_vector[:region_cpt])/float(len(count_vector[:region_cpt]))
+
+        # score is the ratio of avg. signal to right and left of changepoint
+        score = rcpt_avg/lcpt_avg
+
+        # Store in dict keyed by gene name
+        # Consider other options, ie full region entry?
+        changepoint_scores[gene_name] = score
+
+    return top_scores(changepoint_scores, 10)
 
 def calc_changepoint(count_vector):
     """Return first changepoint given an IntVector of counts."""
@@ -45,9 +62,23 @@ def calc_changepoint(count_vector):
     # Can change for multiple (or maybe last?) changepoint
 
     cpointdata = changepoint.cpt_meanvar(count_vector)
-    cpoint = int(changepoint.cpts(cpointdata)[0])  # parse changepoint values
+    cpoints_array = changepoint.cpts(cpointdata)
+
+    if len(cpoints_array) != 0:
+        cpoint = int(changepoint.cpts(cpointdata)[0])  # parse changepoint values
+    else:
+        cpoint = 0    # ie, no changepoint found
 
     return cpoint
+
+def top_scores(score_dict, n):
+    """Return top n score entries from score dictionary"""
+
+    score_list = sorted(score_dict.iteritems(), key=operator.itemgetter(1))
+    score_list.reverse()
+    top_score_list = score_list[:n]
+
+    return top_score_list
 
 def main():
 
